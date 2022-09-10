832a5ecaf4dd196e9a9f6c2b20f8c8e66ab5e64b
thcabrera <thiagomartincabreralavezzi@gmail.com>
2022-09-09 23:42:50 -0300

    Corrijo pom y agrego clase HuellaDeCarbono

---
+++ src/main/java/domain/conversores/LocalDateAttributeConverter.java
@@ -0,0 +1,18 @@
+package domain.conversores;
+
+import javax.persistence.AttributeConverter;
+import java.sql.Date;
+import java.time.LocalDate;
+
+public class LocalDateAttributeConverter implements AttributeConverter<LocalDate, Date> {
+
+    @Override
+    public Date convertToDatabaseColumn(LocalDate localDate) {
+        return localDate == null? null : Date.valueOf(localDate);
+    }
+
+    @Override
+    public LocalDate convertToEntityAttribute(Date date) {
+        return date == null? null : date.toLocalDate();
+    }
+}

cd0bff346908b2615fef482ed279997a2fcd2cdf
thcabrera <thiagomartincabreralavezzi@gmail.com>
2022-09-26 18:01:18 -0300

    Unifico tablas de sector

--- src/main/java/domain/entidades/Municipio.java
+++ src/main/java/domain/entidades/Municipio.java
@@ -1,19 +1,16 @@
 package domain.entidades;
 
-import domain.db.EntityManagerHelper;
 import domain.mediciones.consumos.Periodicidad;
 import lombok.Getter;
 import lombok.Setter;
 import javax.persistence.*;
 import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
 
 // un municipio tiene varias organizaciones
 // no le interesa al municipio saber de que Provincia es
 @Getter
 @Entity
-@Table(name = "municipio")
+@DiscriminatorValue("MUNICIPIO")
 public class Municipio extends Sector{
 
     @Setter
@@ -21,7 +18,7 @@ public class Municipio extends Sector{
     private ArrayList<Organizacion> organizaciones;
 
     @ManyToOne
-    @JoinColumn(name="provincia", referencedColumnName = "id")
+    @JoinColumn(name="provincia_id", referencedColumnName = "id")
     private Provincia provincia;
 
     // ----------------------- CONSTRUCTORES PARA LOS TESTS -------------------------- //
@@ -36,11 +33,6 @@ public class Municipio extends Sector{
 
     // --------------------------------------------------------------------------
 
-    public List<Organizacion> obtenerOrganizaciones(){
-        List<Object> organizaciones = EntityManagerHelper.createQuery("from Organizacion o join Direccion d on o.direccion_id = d.id where d.municipio_id = '" + this.id.toString() + "'").getResultList();
-        return organizaciones.stream().map(o -> (Organizacion) o).collect(Collectors.toList());
-    }
-    
     @Override
     public Double calculoHC(Periodicidad periodo) {
         return this.getOrganizaciones().stream().mapToDouble(org->org.calculoHC(periodo)).sum();
--- src/main/java/domain/entidades/Sector.java
+++ src/main/java/domain/entidades/Sector.java
@@ -6,13 +6,17 @@ import javax.persistence.*;
 
 // de un sector solo nos interesa calcular su huella de carbono, absolutamente nada mas
 @Entity
-@Inheritance(strategy = InheritanceType.JOINED)
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "tipo", length = 10)
 @Table(name = "sector")
 public abstract class Sector {
 
     @Id
     @GeneratedValue
     protected Integer id;
+
+    @Column(name = "descripcion")
+    protected String descripcion;
 
     public abstract Double calculoHC(Periodicidad periodo);
 

b516dcdd3131d7464a5831c47f0e93f813e287f3
thcabrera <thiagomartincabreralavezzi@gmail.com>
2022-10-08 11:36:33 -0300

    us_principal adaptado a handlebars

--- src/main/java/server/Router.java
+++ src/main/java/server/Router.java
@@ -41,5 +41,8 @@ public class Router {
         Spark.path("/organizaciones", ()-> {
             Spark.get("/:id", organizacionesController::mostrar, engine);
         });
+
+        // configure image paths
+        Spark.get("/hola", controllerDefault::saludoController);
     }
 }

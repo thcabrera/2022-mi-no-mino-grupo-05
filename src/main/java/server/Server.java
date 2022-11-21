9a3e2b1701c073e37f8d47eac91da31475492f6c
Augusto Lecaldare <augustolecaldare@gmail.com>
2022-11-20 23:22:36 -0300

    Anda deploy

--- src/main/java/server/Server.java
+++ src/main/java/server/Server.java
@@ -12,10 +12,19 @@ public class Server {
     }
 
     public static void main(String[] args) {
-        Spark.port(9000);
+        Spark.port(getHerokuAssignedPort()); // si queres que sea local reemplazalo por 9000
         Router.init();
         DebugScreen.enableDebugScreen();
         configurar();
+    }
+
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
     }
 
 
